4aa9754001556d6610970314059b02adb56cb645
Rémy TORRES <remytorres27@gmail.com>
2018-11-30 00:16:49 +0100

    add echo

--- node/update.sh
+++ node/update.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+echo "Start update"
+echo "---"
 docker stop helloworld
 docker rm helloworld
 
@@ -7,3 +9,5 @@ docker rmi node:helloworld --force
 
 docker build -t node:helloworld .
 docker run -p 80:80 --name helloworld -d node:helloworld
+echo "---"
+echo "End update"
